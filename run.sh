43d25b60d9b33ea9e436a9517e41c21bc1efc416
Dethree <87226621+Dethree@users.noreply.github.com>
2021-08-03 14:49:29 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+pkg install python
+clear
+apt install python3.9
+clear
+apt install pip3
+clear
+pip3 install pyrogram
+clear
+pip3 install tgcrypto
+clear
+pip3 install asyncio
+clear
+cd tg_gosu
+python3 run.py
